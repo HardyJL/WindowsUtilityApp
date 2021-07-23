ad9a044b82e0c628e97b25111b8740bbaebff472
Johannes Hardenberg <j.hardenberg.adm@grimme.de>
2021-07-23 12:14:28 +0200

    initial commit

---
+++ GeneralComputing/ViewModel/BaseViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GeneralComputing.ViewModel
+{
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private bool loading = false;
+
+        public bool Loading
+
+        {
+            get { return loading; }
+
+            set { SetProperty(ref loading, value); }
+        }
+
+        private string title;
+
+        public string Title
+
+        {
+            get { return title; }
+
+            set { SetProperty(ref title, value); }
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+
+            [CallerMemberName] string propertyName = "",
+
+            Action onChanged = null)
+
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+
+                return false;
+
+            backingStore = value;
+
+            onChanged?.Invoke();
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+
+        {
+            var changed = PropertyChanged;
+
+            if (changed == null)
+
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyPropertyChanged
+    }
+}
